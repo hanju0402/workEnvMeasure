36bdbdaee9bd877447531552fae7f08aea5f6ae9
DESKTOP-F5MR2TE\USER <nahanju0402@naver.com>
2023-12-27 17:30:44 +0900

    물질 MSDS 다건 조회 api 개발 물질명 까지 나오게함 에러 방어로직 적용

--- server.js
+++ server.js
@@ -1,36 +1,150 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
+const key = require("./config/key");
+const { parseString } = require("xml2js");
+
+const serviceKey = key.serviceKey;
+
+const endPoint = "http://msds.kosha.or.kr/openapi/service/msdschem/";
 
 app.listen(8080, function () {
-  console.log('listening on 8080')
+    console.log("listening on 8080");
 });
 
 app.use(express.json());
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "workEnv/build")));
 
-app.use(express.static(path.join(__dirname, 'workEnv/build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/workEnv/build/index.html'));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/workEnv/build/index.html"));
 });
 
-app.get('/api/msdscheck', function (req, res) {
-  axios.get(`http://msds.kosha.or.kr/openapi/service/msdschem/chemlist?searchCnd=1&searchWrd=7440-02-0 &ServiceKey=55ZJREry%2F8ZdOgvqUoqSXswHErF9CHyH73gBMzQUTPJi4g88rctbqWRZLCcY%2FoJ32QdEuLiY6tpdRKc6vllvEQ%3D%3D`)
-    .then(response => {
-      // Handle the response data as needed
-      console.log(response.data);
-    })
-    .catch(error => {
-      // Handle errors
-      console.error('Error:', error);
+const parseStringPromise = (xmlString) => {
+    return new Promise((resolve, reject) => {
+        parseString(xmlString, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
     });
+};
+
+app.get("/api/msdscheck", async function (req, res) {
+    const casNos = req.query.casNo;
+    const promiseArray = [];
+
+    for (const casNo of casNos) {
+        let chemName = "";
+        let chemCas = "";
+        let measurementYn = "N";
+        let measurementCycle = "";
+        let healthCheckYn = "N";
+        let healthCheckCycle = "";
+        let specialYn = "N";
+        let findYn = "N";
+        let chemId = "";
+
+        try {
+            const response = await axios.get(`${endPoint}chemlist?searchCnd=1&searchWrd=${casNo}&ServiceKey=${serviceKey}`);
+            const xmlString = response.data;
+
+            const result = await parseStringPromise(xmlString);
+
+
+            console.log("뭔데?", 'item' in result.response.body[0]);
+            // casNo 속성이 존재하는지 확인
+            if (result.response.body && result.response.body[0].items && result.response.body[0].items[0] && 'item' in result.response.body[0].items[0]) {
+              chemCas = result.response.body[0].items[0].item[0].casNo[0];
+              chemName = result.response.body[0].items[0].item[0].chemNameKor[0];
+              chemId = result.response.body[0].items[0].item[0].chemId[0];
+            } else {
+              chemCas = null; // 또는 다른 기본값으로 설정
+              chemName = ""; // 또는 다른 기본값으로 설정
+              chemName = ""; // 또는 다른 기본값으로 설정
+            }
+
+            console.log("chemCas?", chemCas)
+            console.log("casNo?", casNo)
+            console.log("chemCas == casNo?", chemCas == casNo)
+            if (chemCas == casNo) {
+                const secondResponse = await axios.get(`${endPoint}chemdetail15?chemId=${chemId}&ServiceKey=${serviceKey}`);
+                const secondXmlString = secondResponse.data;
+
+                const secondResult = await parseStringPromise(secondXmlString);
+
+                const itemD = secondResult.response.body[0].items[0].item[0].itemDetail[0];
+                console.log("법", itemD);
+
+                // 측정여부 추출
+                const measureYnMatch = itemD.match(/작업환경측정대상물질/);
+                measurementYn = measureYnMatch ? "Y" : "N";
+
+                // 검진여부 추출
+                const helthCheckYnMatch = itemD.match(/특수건강진단대상물질/);
+                healthCheckYn = helthCheckYnMatch ? "Y" : "N";
+
+                // 측정주기 추출
+                const measurePeriodMatch = itemD.match(/측정주기 : (\d+)개월/);
+                measurementCycle = measurePeriodMatch ? measurePeriodMatch[1] : "";
+
+                // 진단주기 추출
+                const diagnosePeriodMatch = itemD.match(/진단주기 : (\d+)개월/);
+                healthCheckCycle = diagnosePeriodMatch ? diagnosePeriodMatch[1] : "";
+
+                // 특별관리물질 여부 추출
+                const specialYnMatch = itemD.match(/특별관리물질/);
+                specialYn = specialYnMatch ? "Y" : "N";
+
+                findYn = "Y"
+
+                promiseArray.push({
+                    "chemName": chemName,
+                    "chemCas": chemCas,
+                    "measurementYn": measurementYn,
+                    "measurementCycle": measurementCycle,
+                    "healthCheckYn": healthCheckYn,
+                    "healthCheckCycle": healthCheckCycle,
+                    "specialYn": specialYn,
+                    "findYn": findYn
+                });
+            } else {
+                promiseArray.push({
+                  "chemName": chemName,
+                  "chemCas": chemCas,
+                  "measurementYn": measurementYn,
+                  "measurementCycle": measurementCycle,
+                  "healthCheckYn": healthCheckYn,
+                  "healthCheckCycle": healthCheckCycle,
+                  "specialYn": specialYn,
+                  "findYn": findYn
+              });
+            }
+        } catch (error) {
+            // Handle errors
+            promiseArray.push({
+              "chemName": chemName,
+              "chemCas": chemCas,
+              "measurementYn": measurementYn,
+              "measurementCycle": measurementCycle,
+              "healthCheckYn": healthCheckYn,
+              "healthCheckCycle": healthCheckCycle,
+              "specialYn": specialYn,
+              "findYn": findYn
+          });
+            console.error("Error:", error);
+        }
+    }
+
+    console.log("최종데이터리스트 => ", promiseArray);
+    res.json(promiseArray);
 });
 
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/workEnv/build/index.html'));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/workEnv/build/index.html"));
 });
--- workEnv/src/component/MsdsCheck.js
+++ workEnv/src/component/MsdsCheck.js
@@ -1,32 +1,78 @@
-import { useState } from "react";
-import { Col, Container, Row, Table } from "react-bootstrap";
-import axios from 'axios';
-
+import React, { useState } from "react";
+import axios from "axios";
+import '../App.css';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 export default function MsdsCheck() {
-    let [casNo, setCasNo] = useState('');
+    const [casNo, setCasNo] = useState([""]);
+    const [inputCount, setInputCount] = useState(1);
+    const [msdsInfo, setMsdsInfo] = useState([{chemName: "", chemCas:"", measurementYn:"", measurementCycle:"", healthCheckYn:"", healthCheckCycle:"", specialYn:"", findYn:""}]);
 
-    function link() {
-        axios.get(`http://localhost:8080/api/msdscheck?casNo=${casNo}`)
-            .then(response => {
-                // Handle the response data as needed
-                console.log(response.data);
+    const handleInputChange = (index, value) => {
+        if (/^[0-9-]*$/.test(value)) {
+            const newCasNos = [...casNo];
+            newCasNos[index] = value;
+            setCasNo(newCasNos);
+        } else {
+            window.alert("숫자와 '-'만 입력할 수 있습니다.");
+        }
+    };
+
+    const addInput = () => {
+        setInputCount(inputCount + 1);
+        setCasNo([...casNo, ""]);
+        setMsdsInfo([...msdsInfo, ""]); // 각 인풋 박스에 대한 데이터를 빈 문자열로 초기화
+    };
+
+    const removeInput = (index) => {
+        const newCasNos = [...casNo];
+        const newMsdsInfo = [...msdsInfo];
+        newCasNos.splice(index, 1);
+        newMsdsInfo.splice(index, 1);
+        setCasNo(newCasNos);
+        setMsdsInfo(newMsdsInfo);
+        setInputCount(inputCount - 1);
+    };
+
+    const link = () => {
+        axios
+            .get("http://localhost:8080/api/msdscheck", {
+                params: { casNo },
             })
-            .catch(error => {
+            .then((response) => {
+                setMsdsInfo(response.data);
+            })
+            .catch((error) => {
                 // Handle errors
-                console.error('Error:', error);
+                console.error("Error:", error);
             });
-    }
+    };
 
     return (
         <>
+            <div id="addCasNo">
+                <button style={{ marginLeft: "auto" }} onClick={addInput}>
+                    CasNo 추가
+                </button>
+            </div>
             <div>
-                <input onChange={(e) => {
-                    setCasNo(e.target.value);
-                }}/>
-                <button onClick={link}>연동하기</button>
-
+                {Array.from({ length: inputCount }).map((_, index) => (
+                    <div key={index} style={{ marginBottom: "10px" }}>
+                        <input
+                            value={casNo[index]}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
+                            placeholder="CasNo를 입력해주세요"
+                        />
+                        <CloseButton onClick={() => removeInput(index)} id="remove" />
+                        {msdsInfo[index] && msdsInfo[index].findYn=="N" ?  "존재하지 않는 CasNo 입니다." : msdsInfo[index].chemName}
+                    </div>
+                ))}
+            </div>
+            <div></div>
+            <div>
+                <button onClick={link}>물질확인하기</button>
             </div>
         </>
     );
 }
+
